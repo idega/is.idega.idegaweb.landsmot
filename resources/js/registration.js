909a98042a10b264c72bb38b67a69d24a16160c4
Grimur Jonsson <gimmi@idega.com>
2007-06-07 17:34:50 +0000

    *** empty log message ***

---
+++ resources/js/registration.js
@@ -0,0 +1,35 @@
+window.addEvent('domready', function() {
+
+	var inputs = new Array();
+
+	$$('input.personalID').each(
+	function(element, index){
+		inputs.push(element);
+		element.addEvent('keyup', function() {
+		
+			var lastIndex = element.value.length;
+			var lastChar = element.value.charAt(lastIndex-1);
+			
+			if (lastChar == '-') {
+				element.value = element.value.substring(0, lastIndex-1);
+			}
+		
+			if (element.value.length == 10) {
+				LandsmotEventBusiness.getUserNameDWR(element.fillSpan, element.value);
+			} else {
+				element.spaner.innerHTML = '';
+			}
+		});
+		
+		element.fillSpan = function(data) {
+			element.spaner.innerHTML = data;
+		}
+
+	});
+
+	$$('span.userResults').each(
+	function(element, index){
+		inputs[index].spaner = element;
+	});
+	
+});
